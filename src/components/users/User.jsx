808c5ec501551cb84e64a7ec970ac61dabc087e7
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2024-04-08 14:13:47 +0300

    user profile

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -14,9 +14,74 @@ function User() {
     getUser(params.login)
   }, [])
 
-  
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user
 
-  return <div>{user.login}</div>
+  if(loading){
+    return <Spinner />
+  }
+  
+  return (
+    <>
+      <div className="w-full mx-auto lg:w-10/12">
+        <div className="mb-4">
+          <Link to='/' className='btn btn-ghost white-text bold-text'>
+            Back To Search
+          </Link>
+        </div>
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md-cols-3 mb-8 md:gap-8">
+          <div className="custom-card-image mb-6 md:mb-0">
+            <div className="rounded-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt=""/>
+              </figure>
+              <div className="card-body justify-end">
+                <h2 className="card-title mb-0 white-text bold-text">
+                  {name}
+                </h2>
+                <p className="white-text">{login}</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <div className="mb-6">
+              <h1 className="text-3xl card-title">
+                <span className="white-text bold-text">{name}</span>
+                <div className="ml-2 mr-1 badge badge-success">
+                  <span className="white-text bold-text">{type}</span>
+                </div>
+                {hireable && (
+                  <div className="mx-1 badge badge-info white-text bold-text">
+                    Hireable
+                  </div>
+                )}
+              </h1>
+              <p className="white-text bold-text">{bio}</p>
+              <div className="mt-4 card-actions">
+                <a href={html_url} target='_blank' rel='noreferrer' className='btn btn-outline white-text bold-text'>
+                  Visit Github Profile
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default User
