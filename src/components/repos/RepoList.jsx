fe9aa8776eb05e21b99c4b255953dc8c3d7bd6b2
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2024-04-09 09:41:44 +0300

    add to the user repo items

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import RepoItem from './RepoItem'
 
 function RepoList({repos}) {
   if(!repos){
@@ -8,10 +9,10 @@ function RepoList({repos}) {
     <div className='rounded-lg shadow-lg card bg-base-100'>
       <div className="card-body">
         <h2 className="text-3xl my-4 font-bold card-title">
-          Top Repositories
+          Latest Repositories
         </h2>
         {repos.map((repo) => (
-          <h3 key={repo.id}>{repo.name}</h3>
+          <RepoItem key={repo.id} repo={repo} />
         ))}
       </div>
     </div>
